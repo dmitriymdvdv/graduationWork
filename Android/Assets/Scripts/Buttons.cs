f9c61d13781004adde0f36a4e063a25829b3265a
Dima Mevedev <dmitriymdvdv@gmail.com>
2017-05-10 01:22:52 +0300

    Первая простая андройд игра

---
+++ Android/Assets/Scripts/Buttons.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour {
+
+	public Sprite layer_blue, layer_red;
+	public string action;
+
+	void OnMouseDown () {
+		GetComponent<SpriteRenderer> ().sprite = layer_red;
+	}
+
+	void OnMouseUp () {
+		GetComponent<SpriteRenderer> ().sprite = layer_blue;
+	}
+
+	void OnMouseUpAsButton () {
+		switch (gameObject.name) {
+		case "Play":
+			Application.LoadLevel ("play");
+			break;
+		case "Rating":
+			Application.OpenURL ("https://google.com");
+			break;
+		case "Replay":
+			Application.LoadLevel ("play");
+			break;
+
+		case "Home":
+			Application.LoadLevel ("main");
+			break;
+		case "Facebook":
+			Application.OpenURL ("https://facebook.com");
+			break;
+		}
+	}
+}
